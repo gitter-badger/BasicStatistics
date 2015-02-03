d8bf76ef8b7281675f03ce69671cb85c6ec7536f
asantos <aj.santos128@gmail.com>
2015-02-03 16:22:15 -0400

    Declared properties and created the Count method

--- BasicStatistics/Statistics.cs
+++ BasicStatistics/Statistics.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace BasicStatistics
 {
-    public class Statistics
-    {
-		internal static double Sum()
+	public class Statistics
+	{
+		// Declare properties.
+		List<double> numbers;
+		public double Sum { get; set; }
+		public double Min { get; set; }
+		public double Max { get; set; }
+
+		// Initialize numbers list.
+		public Statistics()
 		{
-			throw new NotImplementedException();
+			numbers = new List<double>();
 		}
+
+		public int Count
+		{
+			get { return numbers.Count; }
+		}
+
 	}
 }
